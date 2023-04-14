e19537230561bcd751a25a33e47fcbf0477028d2
cbvfgtn217@gmail.com <cbvfgtn217@gmail.com>
2023-04-14 10:24:31 +0300

    sdgfdfdgdf

--- Calcul/MainWindow.xaml.cs
+++ Calcul/MainWindow.xaml.cs
@@ -20,83 +20,94 @@ namespace Calcul
     /// </summary>
     public partial class MainWindow : Window
     {
-        string leftOperation = "", rightoperation = "", operatoin = "";
+      
 
-        private void CE_Click(object sender, RoutedEventArgs e)
-        {
-            WorkPanel.Text = "";
-            leftOperation = "";
-            rightoperation = "";
-            operatoin = "";
-
-
-
-        }
-
-        private void One_Click(object sender, RoutedEventArgs e)
-        {
-            string ContentBtn = (sender as Button).Content.ToString();
-            WorkPanel.Text += ContentBtn;
-
-            if (ContentBtn == "=")
-            {
-                try
-                {
-                    double num1 = double.Parse(leftOperation);
-                    double num2 = double.Parse(rightoperation);
-                    switch (operatoin)
-                    {
-                        case "+":
-                            WorkPanel.Text = (num1 + num2).ToString();
-                            break;
-                        case "-":
-                            WorkPanel.Text = (num1 - num2).ToString();
-                            break;
-                        case "*":
-                            WorkPanel.Text = (num1 * num2).ToString();
-                            break;
-                        case "/":
-                            WorkPanel.Text = (num1 / num2).ToString();
-                            break;
-                    }
-                    leftOperation = WorkPanel.Text;
-                    rightoperation = "";
-                    operatoin = "";
-                }
-                catch (Exception q)
-                {
-                    WorkPanel.FontSize = 10;
-                    WorkPanel.Text = q.Message;
-                }
-
-
-            }
-
-
-            else
-            {
-                if (leftOperation == "")
-                    leftOperation = ContentBtn;
-                else
-                {
-                    rightoperation = ContentBtn;
-                }
-
-            }
-            if (ContentBtn == "+" || ContentBtn == "-" || ContentBtn == "*" || ContentBtn == "/")
-            {
-                operatoin = ContentBtn;
-
-            }
-            else
-            {
-            }
-
-
-        }
+       
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            tb.Text += b.Content.ToString();
+        }
+
+        private void Result_click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                result();
+            }
+            catch (Exception exc)
+            {
+                tb.Text = "Error!";
+            }
+        }
+
+        private void result()
+        {
+            String op;
+            int iOp = 0;
+            if (tb.Text.Contains("+"))
+            {
+                iOp = tb.Text.IndexOf("+");
+            }
+            else if (tb.Text.Contains("-"))
+            {
+                iOp = tb.Text.IndexOf("-");
+            }
+            else if (tb.Text.Contains("*"))
+            {
+                iOp = tb.Text.IndexOf("*");
+            }
+            else if (tb.Text.Contains("/"))
+            {
+                iOp = tb.Text.IndexOf("/");
+            }
+            else
+            {
+                //error    
+            }
+
+            op = tb.Text.Substring(iOp, 1);
+            double op1 = Convert.ToDouble(tb.Text.Substring(0, iOp));
+            double op2 = Convert.ToDouble(tb.Text.Substring(iOp + 1, tb.Text.Length - iOp - 1));
+
+            if (op == "+")
+            {
+                tb.Text = (op1 + op2).ToString();
+            }
+            else if (op == "-")
+            {
+                tb.Text = (op1 - op2).ToString();
+            }
+            else if (op == "*")
+            {
+                tb.Text = (op1 * op2).ToString();
+            }
+            else
+            {
+                tb.Text = (op1 / op2).ToString();
+            }
+        }
+
+        private void Off_Click_1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+            tb.Text = "";
+        }
+
+        private void R_Click(object sender, RoutedEventArgs e)
+        {
+            if (tb.Text.Length > 0)
+            {
+                tb.Text = tb.Text.Substring(0, tb.Text.Length - 1);
+            }
         }
     }
 }
